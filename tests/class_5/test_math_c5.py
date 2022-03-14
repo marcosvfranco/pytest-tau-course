2dfc36705d9d50ee807df0b20268a871810ec905
Marcos Franco <v.marcos.franco@gmail.com>
2022-03-14 07:47:31 -0300

    task: class 5 exercises

---
+++ tests/class_5/test_math_c5.py
@@ -0,0 +1,16 @@
+
+"""
+from stuff.accum import Accumulator
+
+def accum():
+    return Accumulator()
+
+def test_accumulator_init():
+    accum = Accumulator()
+    assert accum.count == 0
+
+def test_accumulator_count_1():
+    accum = Accumulator()
+    accum.add()
+    assert accum.count == 1
+"""
